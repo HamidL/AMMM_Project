6cc0f09f8b959d6146f812b3c75e0ecc43bb57be
Hamid <hlm6074@gmail.com>
2018-12-18 16:24:17 +0100

    Used classes to store input data

--- Service.py
+++ Service.py
@@ -1,11 +1,13 @@
 class Service(object):
-    def __init__(self, serviceId, passengers, km, startingTime, minutes, overlapping):
+    def __init__(self, serviceId, passengers, km, startingTime, minutes, overlapping, DM, DK):
         self._serviceId = serviceId
         self._passengers = passengers
         self._km = km
         self._startingTime = startingTime
         self._minutes = minutes
         self._overlapping = overlapping
+        self._DM = DM
+        self._DK = DK
 
     def getId(self):
         return self._serviceId
@@ -27,3 +29,9 @@ class Service(object):
 
     def getNumOverlappingServices(self):
         return sum(self._overlapping)
+
+    def getDM(self):
+        return self._DM
+
+    def getDK(self):
+        return self._DK
